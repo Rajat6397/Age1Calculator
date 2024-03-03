96730ba57ff0f75b728f7a614534a28211436b48
Rajat <rajatsaxena6397@gmail.com>
2024-03-03 23:30:01 +0530

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+const butEl = document.getElementById("but");
+const birthdayEl = document.getElementById("birthday");
+const outputEl = document.getElementById("output");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    outputEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+butEl.addEventListener("click", calculateAge);
